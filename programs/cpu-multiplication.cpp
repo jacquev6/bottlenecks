87fa6fb202304f44fb4db114077c408eb860bf13
Vincent Jacques <vincent@vincent-jacques.net>
2022-02-24 17:34:17 +0100

    C++-ify the previous examples

--- programs/cpu-multiplication.cpp
+++ programs/cpu-multiplication.cpp
@@ -1,12 +1,11 @@
 // Copyright 2022 Vincent Jacques
 
-#include <stdlib.h>
-#include <math.h>
+#include <cstdlib>
 
 
 int main(int argc, char* argv[]) {
   if (argc < 2) exit(1);
-  const int size = 1024 * atoi(argv[1]);
+  const int size = 1024 * std::atoi(argv[1]);
 
   bool ok = true;
 
--- programs/cpu-trigonometry.cpp
+++ programs/cpu-trigonometry.cpp
@@ -1,12 +1,12 @@
 // Copyright 2022 Vincent Jacques
 
-#include <stdlib.h>
-#include <math.h>
+#include <cstdlib>
+#include <cmath>
 
 
 int main(int argc, char* argv[]) {
   if (argc < 2) exit(1);
-  const int size = 1024 * atoi(argv[1]);
+  const int size = 1024 * std::atoi(argv[1]);
 
   bool ok = true;
 
@@ -14,7 +14,7 @@ int main(int argc, char* argv[]) {
   for (int i = 0; i < 1024; ++i) {
     for (int j = 0; j != size; ++j) {
       double x = i * size + j;
-      if (acos(cos(x)) < 0) {
+      if (std::acos(std::cos(x)) < 0) {
         ok = false;
         break;
       }
